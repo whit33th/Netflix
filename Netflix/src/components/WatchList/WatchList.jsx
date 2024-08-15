3fadf928a7f2dae8aed559847e3672234dc4cdfd
whit33th <deniil019@gmail.com>
2024-08-15 18:23:58 +0200

    fix

--- Netflix/src/components/WatchList/WatchList.jsx
+++ Netflix/src/components/WatchList/WatchList.jsx
@@ -26,7 +26,7 @@ function WatchList() {
 	}
 	return (
 		<>
-			{watchList ? <div className={s.empty}>Your list is empty</div> : ''}
+			{watchList.length ? <div className={s.empty}>Your list is empty</div> : ''}
 			<div className={s.gridScreen}>
 				{
 					watchList.map((v) => (

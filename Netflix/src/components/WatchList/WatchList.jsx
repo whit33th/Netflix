b67bbdfc78009426c4db9c83205381669b223e0e
whit33th <deniil019@gmail.com>
2024-08-15 18:04:54 +0200

    fix

--- Netflix/src/components/WatchList/WatchList.jsx
+++ Netflix/src/components/WatchList/WatchList.jsx
@@ -25,24 +25,28 @@ function WatchList() {
 		loadWatchList()
 	}
 	return (
-		
-		<div className={s.gridScreen}>
-			{
-				watchList.map((v) => (
-					<div key={v._id}>
-						<Link to={'/' + v.type + '/' + v.name.replace(/\s+/g, '-')}>
-							<img loading="lazy" src={v.img} alt="" />
-						</Link>
-						<div className={s.info}>
-							<p>{v.name}</p>
-							<SmallButton cb={() => deleteToWatchLater(v._id)}>
-							<i className='bx bx-trash'></i>
-							</SmallButton>
+		<>
+			{watchList ? <div className={s.empty}>Your list is empty</div> : ''}
+			<div className={s.gridScreen}>
+				{
+					watchList.map((v) => (
+						<div key={v._id}>
+							<Link to={'/' + v.type + '/' + v.name.replace(/\s+/g, '-')}>
+								<img loading="lazy" src={v.img} alt="" />
+							</Link>
+							<div className={s.info}>
+								<p>{v.name}</p>
+								<SmallButton cb={() => deleteToWatchLater(v._id)}>
+									<i className='bx bx-trash'></i>
+								</SmallButton>
+							</div>
 						</div>
-					</div>
-				))
-			}
-		</div>
+					))
+				}
+			</div>
+		</>
+
+
 	)
 }
 export default WatchList
